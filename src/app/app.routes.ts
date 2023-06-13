59af7129a5b7861bcd630f9506c9162cf41e32de
yube01 <yubraja008@gmail.com>
2023-06-13 20:14:18 +0545

    landling page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'login',loadComponent:()=>import('./components/login/login.component').then((com)=>com.LoginComponent)
+  },
+  {
+    path:'register',loadComponent:()=>import('./components/register/register.component').then((com)=>com.RegisterComponent)
+  },
+  {
+    path:'dashboard',loadComponent:()=>import('./page/dashboard/dashboard.component').then((com)=>com.DashboardComponent)
+  },
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'**',redirectTo:"home",pathMatch:'full'
+  },
+
+
+];
